01923aaca249381c81d90fdffa2b869ddf1d3579
Andrew Bishop <andrew@bishop4.com>
2017-12-13 19:49:43 -0800

    add dummy output

---
+++ Utils/NullOutput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+	public class NullOutput : IOutput
+	{
+		public void BeginDay(int day)
+		{
+		}
+
+		public void BeginPart(int part)
+		{
+		}
+
+		public void BeginTest(int test)
+		{
+		}
+
+		public void EndDay()
+		{
+		}
+
+		public void EndPart(object result)
+		{
+		}
+
+		public void EndTest(bool success, object result, object expected)
+		{
+		}
+	}
+}
