614091f787b1dcd2163727dfa929c2c7f0e64a67
Andrew Bishop <andrew@bishop4.com>
2017-12-15 14:17:30 -0800

    Another change

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -3,18 +3,46 @@ using Utils;
 
 namespace Runner
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-			var day = new Day8.Day8();
-			var output = new ConsoleOutput();
-			output.BeginDay(6);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
 
-			day.Part1(output);
-			day.Part2(output);
+		static void Solve<TInput, TOutput> (INewDay<TInput, TOutput> day)
+		{
+			var output = new ConsoleOutput();
+			var data = day.Data;
+			output.BeginDay(day.Day);
+
+			output.BeginPart(1);
+
+			// part 1
+
+			// test cases
+			foreach (var testCase in data.Part1Tests)
+			{
+				Test.Verify<TInput, TOutput>(output, day.SolvePart1, testCase.Input, testCase.Output);
+			}
+
+			// actual problem
+			var result1 = day.SolvePart1(data.Input);
+			output.EndPart(result1);
+
+
+			// part 2
+
+			// test cases
+			foreach (var testCase in data.Part2Tests)
+			{
+				Test.Verify<TInput, TOutput>(output, day.SolvePart2, testCase.Input, testCase.Output);
+			}
+
+			// actual problem
+			var result2 = day.SolvePart1(data.Input);
+			output.EndPart(result2);
 
 			output.EndDay();
 		}
-    }
+	}
 }
--- Utils/INewDay.cs
+++ Utils/INewDay.cs
@@ -6,6 +6,8 @@ namespace Utils
 {
 	public interface INewDay<TInput, TOutput>
 	{
+		int Day { get; }
+		IData<TInput, TOutput> Data { get; }
 		TOutput SolvePart1(TInput input);
 		TOutput SolvePart2(TInput input);
 	}
