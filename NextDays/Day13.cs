9402160b0d30b5100e1be05ba37e553b7abad59b
Andrew Bishop <andrew@bishop4.com>
2017-12-15 14:32:18 -0800

    Add data base class for cleaner

--- NextDays/Day13.cs
+++ NextDays/Day13.cs
@@ -3,14 +3,18 @@ using Utils;
 
 namespace NextDays
 {
-	public class Day13 : IDay
+	public class Day13 : INewDay<string, int>
 	{
-		public void Part1(IOutput output)
+		public int Day => 13;
+
+		public IData<string, int> Data => throw new NotImplementedException();
+
+		public int SolvePart1(string input)
 		{
 			throw new NotImplementedException();
 		}
 
-		public void Part2(IOutput output)
+		public int SolvePart2(string input)
 		{
 			throw new NotImplementedException();
 		}
---
+++ Utils/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+	public abstract class Data<TInput, TOutput> : IData<TInput, TOutput>
+	{
+		private List<TestData<TInput, TOutput>> part1Tests = new List<TestData<TInput, TOutput>>();
+		private List<TestData<TInput, TOutput>> part2Tests = new List<TestData<TInput, TOutput>>();
+
+		public abstract TInput Input { get; }
+
+		public IEnumerable<TestData<TInput, TOutput>> Part1Tests => part1Tests;
+
+		public IEnumerable<TestData<TInput, TOutput>> Part2Tests => part2Tests;
+
+		protected void AddPart1TestCase(TInput input, TOutput output)
+		{
+			part1Tests.Add(new TestData<TInput, TOutput>(input, output));
+		}
+
+		protected void AddPart2TestCase(TInput input, TOutput output)
+		{
+			part1Tests.Add(new TestData<TInput, TOutput>(input, output));
+		}
+	}
+}
